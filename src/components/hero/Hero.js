426a7c2bd86d21a709705597e1ad37c0db168641
Sergio Dominguez1 <sadoming@bancolombia.com.co>
2023-02-21 09:27:34 -0500

    made changes on components

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Carousel from 'react-material-ui-carousel'
+import { Paper } from '@mui/material'
+import './Hero.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+
+
+const Hero = ({movies}) => {
+  return (
+    <div>
+        <Carousel>
+            {
+                movies?.map((movie) => {
+                    return(
+                        <Paper>
+                            <div className='movie-card-container'>
+                                <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
+                                    <div className='movie-detail'>
+                                        <div className='movie-poster'>
+                                            <img src={movie.poster} alt=" "/>
+                                        </div>
+                                        <div className='movie-title'>
+                                            <h4>{movie.title}</h4>
+                                        </div>
+                                        <div className='movie-button-container'>
+                                            <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.lenght - 11)}`}>
+                                            <div className='play-button-icon-container'>
+                                                <FontAwesomeIcon className='play-button-icon'
+                                                    icon={faCirclePlay}
+                                                />
+                                            </div>
+                                            </Link>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </Paper>
+                    )
+                })
+            }
+        </Carousel>
+    </div>
+  )
+}
+
+export default Hero
